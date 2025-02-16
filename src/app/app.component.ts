8bd648e0d6476b204df0a62ca9d735c943d00951
Estif Fabian Arenas Pamplona <fabian.arenas@inovtech.com.mx>
2025-02-15 22:50:48 -0500

    add app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet] // Importa RouterOutlet para que reconozca <router-outlet>
+})
+export class AppComponent {}
