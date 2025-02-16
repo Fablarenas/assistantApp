3430a1310a8215ac53f67470e7f9df05daf00782
Estif Fabian Arenas Pamplona <fabian.arenas@inovtech.com.mx>
2025-02-15 23:46:14 -0500

    DELETE TEST

--- src/app/chatbot/components/auth-callback-component/auth-callback-component.component.ts
+++ src/app/chatbot/components/auth-callback-component/auth-callback-component.component.ts
@@ -10,22 +10,17 @@ export class AuthCallbackComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    // Captura el token de los parámetros de la URL
     this.route.queryParams.subscribe(params => {
       const token = params['token'];
       const rol = params['rol'];
       if (token) {
-        // Verifica que el token esté siendo recibido correctamente
         console.log('Token recibido:', token);
 
-        // Guarda el rol y token en localStorage
         localStorage.setItem('access_token', token);
         localStorage.setItem('rol', rol);
 
-        // Redirige al usuario a la página principal
         this.router.navigate(['/']);
       } else {
-        // Si no hay token, redirige a login
         this.router.navigate(['/login']);
       }
     });
--- src/app/chatbot/services/assistan-service.service.ts
+++ src/app/chatbot/services/assistan-service.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http'; 
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AssistantResponse } from './interfaces/assistant-response';
+import { environment } from '../../../environments/environment';
 
 interface ChatRequest {
   role: string;
@@ -11,19 +12,17 @@ interface ChatRequest {
 @Injectable()
 export class AssistanServiceService {
 
-  private url: string = `http://localhost:8000/question`;
-  private uploadUrl: string = `http://localhost:8000/upload/`;
-  private askUrl: string = `http://localhost:8000/chat/`;
+  private url: string = `${environment.apiUrl}/question`;
+  private uploadUrl: string = `${environment.apiUrl}/upload/`;
+  private askUrl: string = `${environment.apiUrl}/chat/`;
 
   constructor(private httpClient: HttpClient) {}
 
   getAnswer(questions: string): Observable<AssistantResponse> {
-    const params = new HttpParams()
-      .set('question', questions);
+    const params = new HttpParams().set('question', questions);
     return this.httpClient.get<AssistantResponse>(this.url, { params });
   }
 
-  // Método para subir un archivo
   uploadFile(file: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
@@ -47,7 +46,6 @@ export class AssistanServiceService {
       eventSource.onmessage = (event) => {
         console.log('Chunk recibido:', event.data);
         
-        // Si el backend envía un evento de cierre, cerrar la conexión
         if (event.data === '[END]') {
           console.log('Fin de la transmisión recibido. Cerrando EventSource.');
           observer.complete();
@@ -69,7 +67,4 @@ export class AssistanServiceService {
       };
     });
   }
-  
-  
-  
 }
