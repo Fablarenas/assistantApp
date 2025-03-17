1e4272854249477db51a07088a35bd13282d1c17
Estif Fabian Arenas Pamplona <fabian.arenas@inovtech.com.mx>
2025-03-17 00:19:10 -0500

    enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://assistantapi-app.bluecoast-f651fdb7.centralus.azurecontainerapps.io'
+    apiUrl: 'https://assistantapi.calmdesert-5b60c55f.centralus.azurecontainerapps.io'
   };
